0535fadcaebfae59f8c4b9fb784d080860b85f97
cbHasib <hasibul.pbn@gmail.com>
2023-02-17 19:42:43 +0600

    Update cta

--- src/Pages/Home/components/CTAComponent/CTAComponent.config.js
+++ src/Pages/Home/components/CTAComponent/CTAComponent.config.js
@@ -1,3 +1,7 @@
 import ctabg from "../../../../assets/images/background/ctaBG.jpg";
 
 export const CTABG = ctabg;
+export const CTA_TITLE = "WE SERVE YOU THE BEST";
+export const CTA_SUBTITLE =
+  "We servce the best food to our customers and we are the best in the town";
+export const CTA_BUTTON_TEXT = "Download App";
--- src/Pages/Home/components/CTAComponent/CTAComponent.jsx
+++ src/Pages/Home/components/CTAComponent/CTAComponent.jsx
@@ -1,4 +1,9 @@
-import { CTABG } from "./CTAComponent.config";
+import {
+  CTABG,
+  CTA_BUTTON_TEXT,
+  CTA_SUBTITLE,
+  CTA_TITLE,
+} from "./CTAComponent.config";
 
 const CTAComponent = () => {
   return (
@@ -11,17 +16,14 @@ const CTAComponent = () => {
       }}
     >
       <div className="bg-black/50 w-full h-full">
-        <div className="flex flex-col justify-center text-center py-24 max-w-7xl mx-auto items-center">
-          <h2 className="text-5xl font-semibold text-white">
-            WE SERVE YOU THE BEST
+        <div className="flex flex-col justify-center text-center py-24 max-w-7xl mx-auto items-center px-5">
+          <h2 className="text-4xl md:text-5xl font-semibold text-white">
+            {CTA_TITLE}
           </h2>
-          <span className="w-20 h-1 bg-primary block mt-2 mb-3"></span>
-          <p className="text-gray-300">
-            We servce the best food to our customers and we are the best in the
-            town
-          </p>
+          <span className="w-32 h-1 bg-primary block my-3"></span>
+          <p className="text-gray-300">{CTA_SUBTITLE}</p>
           <button className="bg-primary hover:bg-secondary duration-300 px-7 py-2.5 mt-4 text-white font-semibold rounded-lg">
-            Download App
+            {CTA_BUTTON_TEXT}
           </button>
         </div>
       </div>
